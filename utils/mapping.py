3128eee857953654af3055be7d6a35ee54db34b9
kimjy-st <jy_kim@seoultech.ac.kr>
2026-02-13 01:31:26 +0900

    Features: Add pseudo query embedding path

--- utils/mapping.py
+++ utils/mapping.py
@@ -2,41 +2,51 @@ DATASETMAP = {
     'arxiv': {
         'train' : 'arxivqa_test_subsampled_train.npz',
         'test' : 'arxivqa_test_subsampled_test.npz',
-        'mf5' : 'compressed/mf5/arxivqa_test_subsampled_dump_mf5_compressed.npz',
-        'mf10' : 'compressed/mf10/arxivqa_test_subsampled_dump_mf10_compressed.npz',
-        'mf25' : 'compressed/mf25/arxivqa_test_subsampled_dump_mf25_compressed.npz',
-        'mf50' : 'compressed/mf50/arxivqa_test_subsampled_dump_mf50_compressed.npz'
+        'mf5' : 'mf5/arxivqa_test_subsampled_dump_mf5_compressed.npz',
+        'mf10' : 'mf10/arxivqa_test_subsampled_dump_mf10_compressed.npz',
+        'mf25' : 'mf25/arxivqa_test_subsampled_dump_mf25_compressed.npz',
+        'mf50' : 'mf50/arxivqa_test_subsampled_dump_mf50_compressed.npz',
+        'split_before' : 'arxivqa_test_subsampled_dump_new.npz',
+        'pseudoQ' : 'arxivqa_test_subsampled_query.npz'
     },
     'docvqa': {
         'train' : 'docvqa_test_subsampled_train.npz',
         'test' : 'docvqa_test_subsampled_test.npz',
-        'mf5' : 'compressed/mf5/docvqa_test_subsampled_dump_mf5_compressed.npz',
-        'mf10' : 'compressed/mf10/docvqa_test_subsampled_dump_mf10_compressed.npz',
-        'mf25' : 'compressed/mf25/docvqa_test_subsampled_dump_mf25_compressed.npz',
-        'mf50' : 'compressed/mf50/docvqa_test_subsampled_dump_mf50_compressed.npz'
+        'mf5' : 'mf5/docvqa_test_subsampled_dump_mf5_compressed.npz',
+        'mf10' : 'mf10/docvqa_test_subsampled_dump_mf10_compressed.npz',
+        'mf25' : 'mf25/docvqa_test_subsampled_dump_mf25_compressed.npz',
+        'mf50' : 'mf50/docvqa_test_subsampled_dump_mf50_compressed.npz',
+        'split_before' : 'docvqa_test_subsampled_new.npz',
+        'pseudoQ':'docvqa_test_subsampled_query.npz'
     },
     'infovqa': {
         'train' : 'infovqa_test_subsampled_train.npz',
         'test' : 'infovqa_test_subsampled_test.npz',
-        'mf5' : 'compressed/mf5/infovqa_test_subsampled_dump_mf5_compressed.npz',
-        'mf10' : 'compressed/mf10/infovqa_test_subsampled_dump_mf10_compressed.npz',
-        'mf25' : 'compressed/mf25/infovqa_test_subsampled_dump_mf25_compressed.npz',
-        'mf50' : 'compressed/mf50/infovqa_test_subsampled_dump_mf50_compressed.npz'
+        'mf5' : 'mf5/infovqa_test_subsampled_dump_mf5_compressed.npz',
+        'mf10' : 'mf10/infovqa_test_subsampled_dump_mf10_compressed.npz',
+        'mf25' : 'mf25/infovqa_test_subsampled_dump_mf25_compressed.npz',
+        'mf50' : 'mf50/infovqa_test_subsampled_dump_mf50_compressed.npz',
+        'split_before' : 'infovqa_test_subsampled_new.npz',
+        'pseudoQ':'infovqa_test_subsampled_query.npz'
     },
     'tabfquad': {
         'train' : 'tabfquad_test_subsampled_train.npz',
         'test' : 'tabfquad_test_subsampled_test.npz',
-        'mf5' : 'compressed/mf5/tabfquad_test_subsampled_dump_mf5_compressed.npz',
-        'mf10' : 'compressed/mf10/tabfquad_test_subsampled_dump_mf10_compressed.npz',
-        'mf25' : 'compressed/mf25/tabfquad_test_subsampled_dump_mf25_compressed.npz',
-        'mf50' : 'compressed/mf50/tabfquad_test_subsampled_dump_mf50_compressed.npz'
+        'mf5' : 'mf5/tabfquad_test_subsampled_dump_mf5_compressed.npz',
+        'mf10' : 'mf10/tabfquad_test_subsampled_dump_mf10_compressed.npz',
+        'mf25' : 'mf25/tabfquad_test_subsampled_dump_mf25_compressed.npz',
+        'mf50' : 'mf50/tabfquad_test_subsampled_dump_mf50_compressed.npz',
+        'split_before' : 'tabfquad_test_subsampled_new.npz',
+        'pseudoQ':'tabfquad_test_subsampled_query.npz'
     },
     'tatdqa': {
         'train' : 'tatdqa_test_train.npz',
         'test' : 'tatdqa_test_test.npz',
-        'mf5' : 'compressed/mf5/tatdqa_test_dump_mf5_compressed.npz',
-        'mf10' : 'compressed/mf10/tatdqa_test_dump_mf10_compressed.npz',
-        'mf25' : 'compressed/mf25/tatdqa_test_dump_mf25_compressed.npz',
-        'mf50' : 'compressed/mf50/tatdqa_test_dump_mf50_compressed.npz'
+        'mf5' : 'mf5/tatdqa_test_dump_mf5_compressed.npz',
+        'mf10' : 'mf10/tatdqa_test_dump_mf10_compressed.npz',
+        'mf25' : 'mf25/tatdqa_test_dump_mf25_compressed.npz',
+        'mf50' : 'mf50/tatdqa_test_dump_mf50_compressed.npz',
+        'split_before' : 'tatdqa_test_new.npz',
+        'pseudoQ':'tatdqa_test_query.npz'
     }
 }
